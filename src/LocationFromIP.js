6060d6be2020ec1772c1ce6e19c0f5ca3a07eca7
Mike Wagner <mvw5669@psu.edu>
2022-01-31 19:57:56 -0500

    lab 3

--- src/LocationFromIP.js
+++ src/LocationFromIP.js
@@ -1,6 +1,7 @@
 // dependencies / things imported
 import { LitElement, html, css } from 'lit';
 import { UserIP } from './UserIP.js';
+import '@lrnwebcomponents/wikipedia-query';
 
 export class LocationFromIP extends LitElement {
   static get tag() {
@@ -19,6 +20,8 @@ export class LocationFromIP extends LitElement {
     return {
       lat: { type: Number },
       long: { type: Number },
+      city: { type: String },
+      state: { type: String },
     };
   }
 
@@ -43,6 +46,8 @@ export class LocationFromIP extends LitElement {
         console.log(data);
         this.long = data.longitude;
         this.lat = data.latitude;
+        this.city = data.city;
+        this.state = data.region_name;
         return data;
       });
   }
@@ -65,7 +70,15 @@ export class LocationFromIP extends LitElement {
     // this function runs every time a properties() declared variable changes
     // this means you can make new variables and then bind them this way if you like
     const url = `https://maps.google.com/maps?q=${this.lat},${this.long}&t=&z=15&ie=UTF8&iwloc=&output=embed`;
-    return html`<iframe title="Where you are" src="${url}"></iframe> `;
+    return html`
+      <iframe title="Where you are" src="${url}"></iframe>
+      <a href="https://www.google.com/maps/@${this.lat},${this.long},14z"
+        >See on Google</a
+      >
+      <wikipedia-query search="${this.city}, ${this.state}"></wikipedia-query>
+      <wikipedia-query search="${this.city}"></wikipedia-query>
+      <wikipedia-query search="${this.state}"></wikipedia-query>
+    `;
   }
 }
 
